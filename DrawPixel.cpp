6bf0a20f75f6c422487537ffbe48c70bfcaae265
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-07-16 01:44:15 +0900

    [ add , update ] LoadGraphScreen(0, 0, "DxLogo.jpg", TRUE);はこの関数呼び出しをする.cppファイルと同じディレクトリのファイルのみ読み込めるのか........?????????????

--- DrawPixel.cpp
+++ DrawPixel.cpp
@@ -2,12 +2,21 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	ChangeWindowMode(TRUE);//非全画面にセット
+	SetGraphMode(640, 480, 32);//画面サイズ指定
+
 	if (DxLib_Init() == -1)	// DXLibの初期化
 	{
 		return -1;
 	}
 
-	DrawPixel(320, 240, GetColor(255, 255, 255));
+	ClearDrawScreen();
+
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	LoadGraphScreen(0, 0, "DxLogo.jpg", TRUE);
+
+	ScreenFlip();
 
 	WaitKey();
 
