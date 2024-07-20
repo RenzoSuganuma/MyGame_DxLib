ec5228d44b5875c6abdd48034b93d6ff11cdb535
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-07-21 01:35:13 +0900

    [ update ] １つのファイルに集約

--- SarissaEngine/Runtime/SarissaEngine_RuntimeClasses.cpp
+++ SarissaEngine/Runtime/SarissaEngine_RuntimeClasses.cpp
@@ -1,13 +1,16 @@
+#include "SarissaEngine_RuntimeClasses.h"
 #include "Actor.h"
-#include "DxLib.h"
 #include "Component.h"
 
-Actor::Actor() 
+using SarissaEngine::Actor;
+using SarissaEngine::Component;
+
+Actor::Actor()
 {
 	enabled_ = true;
 }
 
-Actor::~Actor() 
+Actor::~Actor()
 {
 	enabled_ = false;
 	attachedComponents_.clear();
@@ -26,8 +29,8 @@ void Actor::Begin()
 
 void Actor::Tick()
 {
-	if(actor_debug)
-	DrawString(0, 10, " Atick ", GetColor(255, 255, 255));
+	if (actor_debug)
+		DrawString(0, 10, " Atick ", GetColor(255, 255, 255));
 
 	auto it = attachedComponents_.begin();
 	while (it != attachedComponents_.end())
@@ -62,5 +65,40 @@ const Actor::AddComponent(Component component)
 void const Actor::RemoveComponent(std::list< Component >::iterator place)
 {
 	attachedComponents_.erase(place);
+}
+
+Component::Component()
+{
+	enabled_ = true;
+}
+
+Component::~Component()
+{
+	attachedActor_ = nullptr;
+	enabled_ = false;
+}
+
+void Component::Begin()
+{
+}
+
+void Component::Tick()
+{
+	if (comp_debug)
+		DrawString(0, 50, " Ctick ", GetColor(255, 255, 255));
+}
+
+void Component::End()
+{
+}
+
+const Actor* const Component::GetActor() const
+{
+	return attachedActor_;
+}
+
+void const Component::SetActor(const Actor* actor)
+{
+	attachedActor_ = actor;
 }
 
---
+++ SarissaEngine/Runtime/SarissaEngine_RuntimeClasses.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "list"
+#include "string"
+#include "DxLib.h"
+
+// SarissaEngine
+
+namespace SarissaEngine
+{
+
+	class Component;
+
+	constexpr bool actor_debug = true;
+
+	class Actor
+	{
+	protected:
+		VECTOR position_{ 0 }, rotation_{ 0 };
+		std::list< Component > attachedComponents_;
+
+	public:
+		std::string name_ = " Actor_ ";
+		bool enabled_ = true;
+
+		Actor();
+		~Actor();
+
+		virtual void Begin();
+		virtual void Tick();
+		virtual void End();
+
+		std::list< Component >::iterator
+			const
+			AddComponent(Component component);
+		void const
+			RemoveComponent(std::list< Component >::iterator place);
+	};
+
+}
+
+namespace SarissaEngine
+{
+
+	constexpr bool comp_debug = true;
+
+	class Actor;
+
+	class Component
+	{
+	protected:
+		const Actor* attachedActor_ = nullptr;
+
+	public:
+		bool enabled_ = true;
+
+		Component();
+		~Component();
+
+		virtual void Begin();
+		virtual void Tick();
+		virtual void End();
+
+		const Actor* const GetActor() const;
+		void const		   SetActor(const Actor* actor);
+	};
+
+}
+
