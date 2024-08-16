7e708b56cd9ce6ad9044ffd2407f79b8694040d0
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-08-16 23:05:28 +0900

    [ update ] 入力キーバインドの変数を実装

--- SarissaEngine/Runtime/SrssEngn_InputHandler.hpp
+++ SarissaEngine/Runtime/SrssEngn_InputHandler.hpp
@@ -5,6 +5,7 @@
 #include "list"
 #include "map"
 
+// 入力デバイスのタイプ
 enum class InputDeviceType
 {
 	Mouse,
@@ -13,11 +14,30 @@ enum class InputDeviceType
 	XInput,
 };
 
-class InputHandler : public SarissaEngine::Runtime::Framework::Component
+// キーボードのキーの列挙型
+enum class KeyboardKey : int
+{
+	A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
+	Num_1, Num_2, Num_3, Num_4, Num_5, Num_6, Num_7, Num_8, Num_9, Num_0,
+	Tab, Shift, Ctrl, Alt,
+};
+
+class InputHandler final : public SarissaEngine::Runtime::Framework::Component
 {
 private:
+#pragma region -キーボード入力まわり-
+	// 入力立ち上がり
+	std::map< KeyboardKey, bool > started_;
+	// 入力が１のとき
+	std::map< KeyboardKey, bool > performed_;
+	// 入力立下り
+	std::map< KeyboardKey, bool > canceled_;
+	// 入力が入っているフレーム数
+	std::map< KeyboardKey, int > performedFrames_;
+#pragma endregion
+
 public:
-	VECTOR moveVec_{0};
+	VECTOR moveVec_{ 0 };
 	const bool const GetInput(int inputAction, InputDeviceType inputType);
 	void Begin_() override;
 	void Tick_(float deltaTime)  override;
