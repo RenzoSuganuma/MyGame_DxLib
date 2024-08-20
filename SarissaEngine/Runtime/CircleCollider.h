e0022aa7e8bf9f30d24bb74937c7cbf6024b3ade
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-08-20 14:32:02 +0900

    [ add ] コライダークラスの追加

---
+++ SarissaEngine/Runtime/CircleCollider.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Collider.h"
+
+namespace SarissaEngine::Runtime::Component
+{
+	// 円のコライダー
+	class CircleCollider final : public Collider
+	{
+	};
+}
---
+++ SarissaEngine/Runtime/Collider.cpp
@@ -0,0 +1,19 @@
+#include "Collider.h"
+
+const bool const Collider::IsOverlapping(const Collider& other)
+{
+
+}
+
+void const Collider::OnBeginOverlap(const Collider& other)
+{
+}
+
+void const Collider::OnStillOverlap(const Collider& other)
+{
+}
+
+void const Collider::OnEndOverlap(const Collider& other)
+{
+}
+
