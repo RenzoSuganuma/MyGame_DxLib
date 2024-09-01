48937c109e364cd13cc4f988290a4f9609cee1ee
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-09-01 20:54:43 +0900

    [ update ] C++ムズイ

--- SarissaEngine/Runtime/SrssEngn_InputHandler.cpp
+++ SarissaEngine/Runtime/SrssEngn_InputHandler.cpp
@@ -22,6 +22,90 @@ const bool const InputHandler::GetInput(int inputAction, InputDeviceType inputTy
 	return ret;
 }
 
+void const InputHandler::CheckKeyboardInput()
+{
+	performed_.insert_or_assign(KeyboardKey::A, CheckHitKey(KEY_INPUT_A));
+	performed_.insert_or_assign(KeyboardKey::B, CheckHitKey(KEY_INPUT_B));
+	performed_.insert_or_assign(KeyboardKey::C, CheckHitKey(KEY_INPUT_C));
+	performed_.insert_or_assign(KeyboardKey::D, CheckHitKey(KEY_INPUT_D));
+	performed_.insert_or_assign(KeyboardKey::E, CheckHitKey(KEY_INPUT_E));
+	performed_.insert_or_assign(KeyboardKey::F, CheckHitKey(KEY_INPUT_F));
+	performed_.insert_or_assign(KeyboardKey::G, CheckHitKey(KEY_INPUT_G));
+	performed_.insert_or_assign(KeyboardKey::H, CheckHitKey(KEY_INPUT_H));
+	performed_.insert_or_assign(KeyboardKey::I, CheckHitKey(KEY_INPUT_I));
+	performed_.insert_or_assign(KeyboardKey::J, CheckHitKey(KEY_INPUT_J));
+	performed_.insert_or_assign(KeyboardKey::K, CheckHitKey(KEY_INPUT_K));
+	performed_.insert_or_assign(KeyboardKey::L, CheckHitKey(KEY_INPUT_L));
+	performed_.insert_or_assign(KeyboardKey::M, CheckHitKey(KEY_INPUT_M));
+	performed_.insert_or_assign(KeyboardKey::N, CheckHitKey(KEY_INPUT_N));
+	performed_.insert_or_assign(KeyboardKey::O, CheckHitKey(KEY_INPUT_O));
+	performed_.insert_or_assign(KeyboardKey::P, CheckHitKey(KEY_INPUT_P));
+	performed_.insert_or_assign(KeyboardKey::Q, CheckHitKey(KEY_INPUT_Q));
+	performed_.insert_or_assign(KeyboardKey::R, CheckHitKey(KEY_INPUT_R));
+	performed_.insert_or_assign(KeyboardKey::S, CheckHitKey(KEY_INPUT_S));
+	performed_.insert_or_assign(KeyboardKey::T, CheckHitKey(KEY_INPUT_T));
+	performed_.insert_or_assign(KeyboardKey::U, CheckHitKey(KEY_INPUT_U));
+	performed_.insert_or_assign(KeyboardKey::V, CheckHitKey(KEY_INPUT_V));
+	performed_.insert_or_assign(KeyboardKey::W, CheckHitKey(KEY_INPUT_W));
+	performed_.insert_or_assign(KeyboardKey::X, CheckHitKey(KEY_INPUT_X));
+	performed_.insert_or_assign(KeyboardKey::Y, CheckHitKey(KEY_INPUT_Y));
+	performed_.insert_or_assign(KeyboardKey::Z, CheckHitKey(KEY_INPUT_Z));
+
+	performed_.insert_or_assign(KeyboardKey::Num_1, CheckHitKey(KEY_INPUT_1));
+	performed_.insert_or_assign(KeyboardKey::Num_2, CheckHitKey(KEY_INPUT_2));
+	performed_.insert_or_assign(KeyboardKey::Num_3, CheckHitKey(KEY_INPUT_3));
+	performed_.insert_or_assign(KeyboardKey::Num_4, CheckHitKey(KEY_INPUT_4));
+	performed_.insert_or_assign(KeyboardKey::Num_5, CheckHitKey(KEY_INPUT_5));
+	performed_.insert_or_assign(KeyboardKey::Num_6, CheckHitKey(KEY_INPUT_6));
+	performed_.insert_or_assign(KeyboardKey::Num_7, CheckHitKey(KEY_INPUT_7));
+	performed_.insert_or_assign(KeyboardKey::Num_8, CheckHitKey(KEY_INPUT_8));
+	performed_.insert_or_assign(KeyboardKey::Num_9, CheckHitKey(KEY_INPUT_9));
+	performed_.insert_or_assign(KeyboardKey::Num_0, CheckHitKey(KEY_INPUT_0));
+
+	performed_.insert_or_assign(KeyboardKey::Shift, CheckHitKey(KEY_INPUT_LSHIFT) || CheckHitKey(KEY_INPUT_RSHIFT));
+	performed_.insert_or_assign(KeyboardKey::Tab, CheckHitKey(KEY_INPUT_TAB));
+	performed_.insert_or_assign(KeyboardKey::Ctrl, CheckHitKey(KEY_INPUT_LCONTROL) || CheckHitKey(KEY_INPUT_RCONTROL));
+	performed_.insert_or_assign(KeyboardKey::Alt, CheckHitKey(KEY_INPUT_LALT) || CheckHitKey(KEY_INPUT_RALT));
+	performed_.insert_or_assign(KeyboardKey::Space, CheckHitKey(KEY_INPUT_SPACE));
+}
+// 入力の 立ち上がり、立下りの入力を更新する
+void const InputHandler::CheckInputStarted() {
+	for (auto item : performed_) {
+		if (item.second) {
+			performedFrames_[item.first]++;
+
+			if (performedFrames_[item.first] < 2) {
+				started_[item.first] = true;
+			}	// 入力フレーム数を調べる
+			else {
+				started_[item.first] = false;
+			}
+		}
+	}	// 入力中の情報をすべてなめる
+}
+// 入力の立下りを調べて状態の更新をする	
+void const InputHandler::CheckInputCanceled() {
+	auto past_it = performedConditionPastFrame_.begin(); // 前フレーム
+	auto current_it = performed_.begin();				 // 現在のフレーム
+	while (current_it != performed_.end()) {
+		if ((*current_it).second == false && (*past_it).second == true) {
+			canceled_[(*current_it).first] = true;
+
+			performedFrames_[(*current_it).first] = 0;
+		}
+		else {
+			canceled_[(*current_it).first] = false;
+		}
+
+		past_it++;
+		current_it++;
+	}
+}
+// 前フレームの入力値を更新
+void const InputHandler::UpdatePastInputValues() {
+	performedConditionPastFrame_ = performed_;	// 値をコピー
+}
+
 const bool const InputHandler::GetInputStarted(const KeyboardKey key)
 {
 	return started_[key];
--- SarissaEngine/Runtime/SrssEngn_InputHandler.hpp
+++ SarissaEngine/Runtime/SrssEngn_InputHandler.hpp
@@ -39,92 +39,14 @@ private:
 	// 入力が入っているフレーム数
 	std::map< const KeyboardKey, int > performedFrames_;
 
-#pragma region -ながーーーーーい処理の関数-
 	// キーボード入力受付処理
-	void const CheckKeyboardInput()
-	{
-		performed_[KeyboardKey::A] = CheckHitKey(KEY_INPUT_A);
-		performed_[KeyboardKey::B] = CheckHitKey(KEY_INPUT_B);
-		performed_[KeyboardKey::C] = CheckHitKey(KEY_INPUT_C);
-		performed_[KeyboardKey::D] = CheckHitKey(KEY_INPUT_D);
-		performed_[KeyboardKey::E] = CheckHitKey(KEY_INPUT_E);
-		performed_[KeyboardKey::F] = CheckHitKey(KEY_INPUT_F);
-		performed_[KeyboardKey::G] = CheckHitKey(KEY_INPUT_G);
-		performed_[KeyboardKey::H] = CheckHitKey(KEY_INPUT_H);
-		performed_[KeyboardKey::I] = CheckHitKey(KEY_INPUT_I);
-		performed_[KeyboardKey::J] = CheckHitKey(KEY_INPUT_J);
-		performed_[KeyboardKey::K] = CheckHitKey(KEY_INPUT_K);
-		performed_[KeyboardKey::L] = CheckHitKey(KEY_INPUT_L);
-		performed_[KeyboardKey::M] = CheckHitKey(KEY_INPUT_M);
-		performed_[KeyboardKey::N] = CheckHitKey(KEY_INPUT_N);
-		performed_[KeyboardKey::O] = CheckHitKey(KEY_INPUT_O);
-		performed_[KeyboardKey::P] = CheckHitKey(KEY_INPUT_P);
-		performed_[KeyboardKey::Q] = CheckHitKey(KEY_INPUT_Q);
-		performed_[KeyboardKey::R] = CheckHitKey(KEY_INPUT_R);
-		performed_[KeyboardKey::S] = CheckHitKey(KEY_INPUT_S);
-		performed_[KeyboardKey::T] = CheckHitKey(KEY_INPUT_T);
-		performed_[KeyboardKey::U] = CheckHitKey(KEY_INPUT_U);
-		performed_[KeyboardKey::V] = CheckHitKey(KEY_INPUT_V);
-		performed_[KeyboardKey::W] = CheckHitKey(KEY_INPUT_W);
-		performed_[KeyboardKey::X] = CheckHitKey(KEY_INPUT_X);
-		performed_[KeyboardKey::Y] = CheckHitKey(KEY_INPUT_Y);
-		performed_[KeyboardKey::Z] = CheckHitKey(KEY_INPUT_Z);
-
-		performed_[KeyboardKey::Num_1] = CheckHitKey(KEY_INPUT_1);
-		performed_[KeyboardKey::Num_2] = CheckHitKey(KEY_INPUT_2);
-		performed_[KeyboardKey::Num_3] = CheckHitKey(KEY_INPUT_3);
-		performed_[KeyboardKey::Num_4] = CheckHitKey(KEY_INPUT_4);
-		performed_[KeyboardKey::Num_5] = CheckHitKey(KEY_INPUT_5);
-		performed_[KeyboardKey::Num_6] = CheckHitKey(KEY_INPUT_6);
-		performed_[KeyboardKey::Num_7] = CheckHitKey(KEY_INPUT_7);
-		performed_[KeyboardKey::Num_8] = CheckHitKey(KEY_INPUT_8);
-		performed_[KeyboardKey::Num_9] = CheckHitKey(KEY_INPUT_9);
-		performed_[KeyboardKey::Num_0] = CheckHitKey(KEY_INPUT_0);
-
-		performed_[KeyboardKey::Shift] = CheckHitKey(KEY_INPUT_LSHIFT) || CheckHitKey(KEY_INPUT_RSHIFT);
-		performed_[KeyboardKey::Tab] = CheckHitKey(KEY_INPUT_TAB);
-		performed_[KeyboardKey::Ctrl] = CheckHitKey(KEY_INPUT_LCONTROL) || CheckHitKey(KEY_INPUT_RCONTROL);
-		performed_[KeyboardKey::Alt] = CheckHitKey(KEY_INPUT_LALT) || CheckHitKey(KEY_INPUT_RALT);
-		performed_[KeyboardKey::Space] = CheckHitKey(KEY_INPUT_SPACE);
-	}
+	void const CheckKeyboardInput();
 	// 入力の 立ち上がり、立下りの入力を更新する
-	void const CheckInputStarted() {
-		for (auto item : performed_) {
-			if (item.second) {
-				performedFrames_[item.first]++;
-
-				if (performedFrames_[item.first] < 2) {
-					started_[item.first] = true;
-				}	// 入力フレーム数を調べる
-				else {
-					started_[item.first] = false;
-				}
-			}
-		}	// 入力中の情報をすべてなめる
-	}
+	void const CheckInputStarted();
 	// 入力の立下りを調べて状態の更新をする	
-	void const CheckInputCanceled() {
-		auto past_it = performedConditionPastFrame_.begin(); // 前フレーム
-		auto current_it = performed_.begin();				 // 現在のフレーム
-		while (current_it != performed_.end()) {
-			if ((*current_it).second == false && (*past_it).second == true) {
-				canceled_[(*current_it).first] = true;
-
-				performedFrames_[(*current_it).first] = 0;
-			}
-			else {
-				canceled_[(*current_it).first] = false;
-			}
-
-			past_it++;
-			current_it++;
-		}
-	}
+	void const CheckInputCanceled();
 	// 前フレームの入力値を更新
-	void const UpdatePastInputValues() {
-		performedConditionPastFrame_ = performed_;	// 値をコピー
-	}
-#pragma endregion
+	void const UpdatePastInputValues();
 #pragma endregion
 
 public:
