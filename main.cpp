b22ddda6f7b0e0b8c114cd152d4f25c2ffa01e30
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-09-01 19:26:00 +0900

    [ remove all ] すべて作り直す

--- main.cpp
+++ main.cpp
@@ -1,31 +1,11 @@
 #include "chrono"
 #include "DxLib.h"
 #include "Windows.h"
-#include "SarissaEngine\Engine\SrssEngn_SoundHandler.hpp"
-#include "SarissaEngine\Engine\SrssEngn_WindowHandler.hpp"
-#include "SarissaEngine\Runtime\SrssEngn_InputHandler.hpp"
-#include "SarissaEngine\Runtime\SrssEngn_Level.hpp"
-#include "SarissaEngine\Runtime\SrssEngn_Actor.hpp"
-#include "SarissaEngine\Runtime\SrssEngn_Component.hpp"
-#include "SarissaEngine\Runtime\SrssEngn_InputHandler.hpp"
-#include "SarissaEngine\Runtime\SrssEngn_CircleCollider.hpp"
-
-#include "Player.h"
-#include "MovingCircle.h"
-
 #pragma comment (lib , "winmm.lib")
-
-/* 【Layer:0】 */
-
-/* ゲームエンジン【ライブラリ】 レイヤ */
-
-/* ここではゲーム本体のコーディングをしない。あくまでもシステム面【エンジン内部】のコードのみ書くこと */
 
 // エントリーポイントを提供
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	using namespace SarissaEngine::Runtime::System;
-
 	// フレームレート制限のための変数群
 	// １フレーム当たりの時間[ms]
 	float frameTime = 0;
@@ -38,50 +18,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	// 経過時間
 	float elapsedTime = 0;
 
-	// Initialize:
-
-	Level* level = new Level;
-
-	// DXLibの初期化
-	if (DxLib_Init() == -1)
-	{
-		return -1;
-	}
-
-	// ウィンドウのセットアップ
-	auto screenSize = GetScreenSize();
-	SetScreenSize(screenSize.first , screenSize.second);
-	SetChangeWindowMode(false);
-	ChangeWindowMode(GetChangeWindowMode());
-	SetRefreshRate(60);
-	frameTime = (1000.0f / (GetRefreshRate()) + 0.5f);
-	SetQuitKey(KEY_INPUT_ESCAPE);
-
-	// テスト用のシーン内セットアップ
-	auto player = new Player;
-	player->SetPlacedLevel(level);
-
-	auto obstacle = new MovingCircle;
-	obstacle->SetPlacedLevel(level);
-
-	InputHandler* input = new InputHandler;
-	player->AddComponent(input);
-
-	CircleCollider* cCollider = new CircleCollider;
-	player->AddComponent(cCollider);
-
-	CircleCollider* obstacleCollider = new CircleCollider;
-	obstacle->AddComponent(obstacleCollider);
-
-	level->AddObject(player);
-	level->AddObject(obstacle);
-
-	// 効果音読み込み
-	int se = LoadSoundToMemory(".\\Resources\\pigeon_se_.mp3");
-
 	ClearDrawScreen();
-	level->MainLoopEntry();
-
+	
 	while (1)
 	{
 		if (ProcessMessage() == -1)
@@ -93,18 +31,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		clockStarted = std::chrono::system_clock::now();
 
 		ClearDrawScreen();
-		DrawFormatStringF(0, screenSize.second - 20, GetColor(255, 255, 255), "Hit Escape Key To Exit : ElapsedTime = %f", elapsedTime);
-
-		level->MainLoopUpdate(deltaTime);
-		level->CollisionUpdate();
+		
 		elapsedTime += deltaTime;
 
-		if (CheckHitKey(GetQuitKey()) == 1)
+		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1)
 		{
-			level->MainLoopExit();
-
-			delete level;
-
 			DxLib_End();
 
 			break;
