161f7080a437703162aeefb358a140c3640883a0
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-10-11 05:02:06 +0900

    no message

---
+++ SarissaEngine/Engine/LangUtil.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+// C#の自動プロパティを再現。左から型、プロパティ名、本体の変数
+#define AutoProperty( type , name , rawValue ) \
+public : const type& get##name() { return rawValue; } \
+void set##name( const type& value ) { rawValue = value; }
+
+// C#のプロパティを再現。左から型、変数名（public）、値を受け渡す前の前処理、値を初期化する前の前処理
+#define Property( type , name , getterbody , setterbody ) \
+public: type name; \
+const type& get##name() { getterbody return name; } \
+void set##name(const type& val) { setterbody name = val; }
